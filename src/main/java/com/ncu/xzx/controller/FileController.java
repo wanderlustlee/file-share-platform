841a2bf9620a2b454f7fe2161eebeb0bdf2548b4
lichao <lichao@bbktel.com>
2020-03-20 14:21:31 +0800

    文件预览功能

--- src/main/java/com/ncu/xzx/controller/FileController.java
+++ src/main/java/com/ncu/xzx/controller/FileController.java
@@ -15,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.Date;
@@ -36,6 +37,12 @@ public class FileController {
 
 //    public static String PATH = "D:\\fileupload";
 
+    /**
+     * 上传
+     * @param token
+     * @param file
+     * @return
+     */
     @PostMapping("/upload")
     @UserLoginToken
     public Response upload(@RequestParam("token") String token, @RequestParam("file") MultipartFile file) {
@@ -80,9 +87,17 @@ public class FileController {
 
     }
 
-    @GetMapping("/download")
+    /**
+     * 下载
+     * @param fileName
+     * @param token
+     * @param request
+     * @param response
+     * @return
+     */
+    @GetMapping("/download/{fileName}")
     @UserLoginToken
-    public Response download(@RequestParam("token") String token, @RequestParam("fileName") String fileName, HttpServletRequest request, HttpServletResponse response) {
+    public Response download(@PathVariable String fileName, @RequestParam("token") String token, HttpServletRequest request, HttpServletResponse response) {
         UserToken userToken = userTokenService.getByToken(token);
         int userId = userToken.getUserId();
         //得到要下载的文件, linux为/  Windows为\\
@@ -95,7 +110,8 @@ public class FileController {
         String realname = fileName.substring(fileName.indexOf("_") + 1);
         try {
             //设置响应头，控制浏览器下载该文件
-            response.setHeader("content-disposition", "attachment;filename=" + URLEncoder.encode(realname, "UTF-8"));
+            response.setHeader("content-disposition", "attachment;filename=" + realname);
+            response.setHeader("content-transfer-encoding","binary");
             //读取要下载的文件，保存到文件输入流
             FileInputStream in = null;
 
@@ -110,9 +126,7 @@ public class FileController {
                 //输出缓冲区的内容到浏览器，实现文件下载
                 out.write(buffer, 0, len);
             }
-            //关闭文件输入流
             in.close();
-            //关闭输出流
             out.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -131,6 +145,60 @@ public class FileController {
         }
 
         return new Response(ResponseCode.OPERATION_ERROR.getStatus(), ResponseCode.OPERATION_ERROR.getMsg(), "");
+
+    }
+
+
+    /**
+     * 文件预览，集成kkfileview  链接：https://gitee.com/kekingcn/file-online-preview/wikis/pages
+     *
+     * 例子：
+     * var originUrl = 'http://10.12.65.57:8888/file/preview/养成活动攻略-2.19.docx';
+     * window.open('http://127.0.0.1:8012/onlinePreview?url='+encodeURIComponent(originUrl));
+     * @param fileName
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping("/preview/{fileName}")
+    public Response preview(@PathVariable String fileName, HttpServletRequest request, HttpServletResponse response) {
+        //得到要下载的文件, linux为/  Windows为\\
+        File file = new File(PATH + File.separator + fileName);
+        //如果文件不存在
+        if (!file.exists()) {
+            return new Response(ResponseCode.OPERATION_ERROR.getStatus(), ResponseCode.OPERATION_ERROR.getMsg(), "");
+        }
+
+        response.reset();
+        //处理文件名
+        String realname = fileName.substring(fileName.indexOf("_") + 1);
+        try {
+            //读取要下载的文件，保存到文件输入流
+            FileInputStream in = null;
+            in = new FileInputStream(PATH + File.separator + fileName);
+
+            fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+            URL u = new URL("file:///" + PATH+fileName);
+            response.setContentType(u.openConnection().getContentType());
+            response.setHeader("Content-Disposition", "inline; filename=" + fileName);
+
+            //创建输出流
+            OutputStream out = response.getOutputStream();
+            //创建缓冲区
+            byte buffer[] = new byte[1024];
+            int len = 0;
+            //循环将输入流中的内容读取到缓冲区当中
+            while ((len = in.read(buffer)) > 0) {
+                //输出缓冲区的内容到浏览器，实现文件下载
+                out.write(buffer, 0, len);
+            }
+            in.close();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new Response("");
 
     }
 }
